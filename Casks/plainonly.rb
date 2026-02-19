8b178a9521a17fa50a2d3f392f6fcf0ba58e175b
조재용 <jaeyoi.dev@gmail.com>
2026-02-19 21:33:44 +0900

    Update PlainOnly to 0.1.0

--- Casks/plainonly.rb
+++ Casks/plainonly.rb
@@ -1,8 +1,8 @@
 cask "plainonly" do
-  version "0.0.0"
-  sha256 "placeholder"
+  version "0.1.0"
+  sha256 "b86aa2aad051a8bd74166cedcdb499124a665e06e3aa3bfb27f4965655f5f117"
 
-  url "placeholder"
+  url "https://github.com/purplitinc/homebrew-apps/releases/download/plainonly-v0.1.0/PlainOnly-0.1.0.zip"
   name "PlainOnly"
   desc "A lightweight macOS text editor designed for clean, distraction-free plain text writing."
   homepage "https://plainonly.purplit.com"
