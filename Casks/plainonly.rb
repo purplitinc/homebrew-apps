ff9d650667e8c44ad5c3cf4abf9ad224b533b114
조재용 <jaeyoi.dev@gmail.com>
2026-02-19 21:31:43 +0900

    Initial commit

---
+++ Casks/plainonly.rb
@@ -0,0 +1,11 @@
+cask "plainonly" do
+  version "0.0.0"
+  sha256 "placeholder"
+
+  url "placeholder"
+  name "PlainOnly"
+  desc "A lightweight macOS text editor designed for clean, distraction-free plain text writing."
+  homepage "https://plainonly.purplit.com"
+
+  app "PlainOnly.app"
+end
